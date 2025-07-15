94aaec12d6d968e674544865ccde1d58d56544be
Phuong Nam <thaidagpnam@gmail.com>
2025-07-15 16:55:49 +0700

    fixing lan cuoi

--- src/main.server.ts
+++ src/main.server.ts
@@ -6,18 +6,15 @@
 
 // export default bootstrap;
 
+// src/main.server.ts
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-export const bootstrap = () => bootstrapApplication(App, config);
+const bootstrap = () => bootstrapApplication(App, config);
 
-/**
- * Đây là cách đúng với Angular SSR mới (dùng `getPrerenderParams`)
- * fix lỗi: "route uses prerendering and includes parameters..."
- */
-export const getPrerenderParams = () => {
+const getPrerenderParams = () => {
     return {
         'detail/:id': [
             { id: '0.141193493107266' },
@@ -27,5 +24,8 @@ export const getPrerenderParams = () => {
     };
 };
 
-// Bắt buộc có để Netlify SSR không lỗi
-export default bootstrap;
+export default {
+    bootstrap,
+    getPrerenderParams
+};
+
