f52901873e15fad61f482148f95030ff5e0a9edd
Phuong Nam <thaidagpnam@gmail.com>
2025-07-15 16:45:36 +0700

    fix get prerenderrouter

--- src/main.server.ts
+++ src/main.server.ts
@@ -10,20 +10,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
-import { Routes } from '@angular/router';
 
-const bootstrap = () => bootstrapApplication(App, config);
+export const bootstrap = () => bootstrapApplication(App, config);
 
-export default {
-    bootstrap,
-    /**
-     * 👇 Hàm này fix lỗi: khai báo các giá trị cụ thể cho route động `detail/:id`
-     */
-    getPrerenderRoutes(): Routes {
-        return [
-            { path: 'detail/0.141193493107266' },
-            { path: 'detail/0.253956475090453' },
-            { path: 'detail/0.666467571051155' }
-        ];
-    }
-};
+export const getPrerenderRoutes = () => [
+    { path: 'detail/0.141193493107266' },
+    { path: 'detail/0.253956475090453' },
+    { path: 'detail/0.666467571051155' }
+];
